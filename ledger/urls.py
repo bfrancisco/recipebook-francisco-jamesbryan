3b58ba4302002c35560d85ff1e9bbcfe5217f5a4
Bryan Francisco <franciscobryaaan@gmail.com>
2024-02-15 18:42:52 +0800

    finished recipe pages

--- ledger/urls.py
+++ ledger/urls.py
@@ -5,8 +5,8 @@ from .views import index, recipe_list, recipe_1, recipe_2
 urlpatterns = [
     path('', index, name='index'),
     path('recipes/list', recipe_list, name='recipes'),
-    path('recipe/1', recipe_1, name='recipes'),
-    path('recipe/2', recipe_2, name='recipes'),
+    path('recipe/1', recipe_1, name='recipe_1'),
+    path('recipe/2', recipe_2, name='recipe_2'),
 ]
 
 app_name = "ledger"
