229f11935a43219d52e9900b1f4da829e43c1d25
Bryan Francisco <franciscobryaaan@gmail.com>
2024-03-07 14:02:31 +0800

    cleaner model functions

--- ledger/models.py
+++ ledger/models.py
@@ -9,7 +9,7 @@ class Ingredient(models.Model):
         return f'{self.name}'
 
     def get_absolute_url(self):
-        return reverse('ledger:ingredient-detail', args=[str(self.pk)])
+        return reverse('ledger:ingredient-detail', args=[self.pk])
     
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
@@ -18,7 +18,7 @@ class Recipe(models.Model):
         return f'{self.name}'
 
     def get_absolute_url(self):
-        return reverse('ledger:recipe-detail', args=[str(self.pk)])
+        return reverse('ledger:recipe-detail', args=[self.pk])
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
