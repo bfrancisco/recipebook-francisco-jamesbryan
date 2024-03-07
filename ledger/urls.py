7d94678979371febb881ea9b433fe4859d94a349
Bryan Francisco <franciscobryaaan@gmail.com>
2024-03-07 13:57:02 +0800

    cleaner URL; removed ledger

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 
-from .views import index, recipes_list, recipe_1, recipe_2, RecipeListView, RecipeDetailView
+from .views import index, RecipeListView, RecipeDetailView
 
 urlpatterns = [
     path('', index, name='index'),
-    # path('recipes/list', recipes_list, name='recipes_list'),
     path('recipes/list', RecipeListView.as_view(), name='recipes_list'),
-    # path('recipe/1', recipe_1, name='recipe_1'),
-    # path('recipe/2', recipe_2, name='recipe_2'),
-    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_<int:pk>')
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe-detail')
 ]
 
 app_name = "ledger"
